74b22e9b20042f9e820503262f6d3580afa4dae7
Will B <will.brackenbury1@gmail.com>
2017-05-14 08:20:04 -0400

    Put general functions in single file

---
+++ k4n50etcTable.py
@@ -0,0 +1,31 @@
+#mainDice.py
+
+import random as rd
+import matplotlib as mtp
+import pickle as pl
+import numpy as np
+from PIL import Image
+import math
+from diceFuncs import *
+
+
+def main():
+
+	
+
+
+
+
+
+
+	return 0
+
+
+
+
+
+
+
+main()
+
+
